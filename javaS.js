61b07bc9e039472d4ddf2156efe204beaff73e47
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-17 21:53:39 +0300

    javaS.js func

--- javaS.js
+++ javaS.js
@@ -1,1 +1,12 @@
+function increase(button) {
+  // Find the <span class="votes"> in the same row
+  let voteSpan = button.parentElement.querySelector(".votes");
+  let current = parseInt(voteSpan.innerText);
+  voteSpan.innerText = current + 1;
+}
 
+function resetVotes(button) {
+  // Find the <span class="votes"> in the same row
+  let voteSpan = button.parentElement.querySelector(".votes");
+  voteSpan.innerText = 0;
+}
